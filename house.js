b60403699cf0ac41d3665fa6fdc2c3baaa4040b3
TheHackerOf2014 <isak.ekelof@gamil.com>
2014-11-16 15:10:58 +0100

    Update house.js

--- house.js
+++ house.js
@@ -1,21 +1,22 @@
-Enter file contents hereexports.ETEGE2 = { 
-2    
-3   upStair: function( size, material ) { 
-4     if( material == undefined ) { 
-5       material = blocks.stairs.stone; 
-6     } 
-7     if( size > 50 ) { 
-8       size = 50; 
-9     } 
-10     return box( material ).fwd().up().times( size ); 
-11   }, 
-12    
-13   downStair: function( size, material ) { 
-14     if( material == undefined ) { 
-15       material = blocks.stairs.stone; 
-16     } 
-17     if( size > 50 ) { 
-18       size = 50; 
-19     } 
-20     return box( material ).fwd().down().times( size ); 
-21   } 
+hereexports.ETEGE2 = { 
+    
+   upStair: function( size, material ) { 
+     if( material == undefined ) { 
+       material = blocks.stairs.stone; 
+     } 
+     if( size > 50 ) { 
+       size = 50; 
+     } 
+     return box( material ).fwd().up().times( size ); 
+   }, 
+    
+   downStair: function( size, material ) { 
+     if( material == undefined ) { 
+       material = blocks.stairs.stone; 
+     } 
+     if( size > 50 ) { 
+       size = 50; 
+     } 
+     return box( material ).fwd().down().times( size ); 
+   }
+}
